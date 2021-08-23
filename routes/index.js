88cab8b8033bab152efd6d667486cc933c79e82c
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-08-23 18:00:47 +0200

    Fetch data from graphQl with axios

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,51 @@
 'use strict';
 const joi = require('joi');
-
-// configure routes will be used in the main server.js file
-// to get the corresponding server.js file.
+const axios = require('axios');
 
 const hello = {
     handler: function (request, h) {
         return 'Hello World!';
+    }
+};
+
+const data = JSON.stringify({
+    query: `query {
+  states{
+    grid
+  } 
+}`,
+    variables: {}
+});
+
+const config = {
+    method: 'post',
+    url: 'http://localhost:4000/',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    data : data
+};
+
+const axiosGql = () => axios(config)
+    .then(function (response) {
+        const states = response.data.data.states
+        const lastIndex = states.length-1
+        const theNewestState = states[lastIndex].grid
+        console.log(theNewestState)
+        return theNewestState
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+const giveState = () => new Promise((resolve, reject) =>{
+    resolve(axiosGql())
+    reject('giveState promise rejected')
+})
+
+const qclState = {
+    handler: async function (request, h) {
+        return await giveState()
     }
 };
 
