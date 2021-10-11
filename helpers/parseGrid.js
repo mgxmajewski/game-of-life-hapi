d2b1335d1f0f9bbb9c3cd7ec57cac07ccc929a45
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-10-11 23:10:56 +0200

    Applies EsLinter to parseGrid helper function.

--- helpers/parseGrid.js
+++ helpers/parseGrid.js
@@ -1,19 +1,22 @@
-const {getLongestRow} = require("./getLongestRow");
-exports.parseGrid = (parsedAxiosData => {
-    const rows = parsedAxiosData.length
-    const columns = getLongestRow(parsedAxiosData)
-    let aliveCells = []
-    for (let row = 0; row<rows; row++){
-        const columns = parsedAxiosData[row].length
-        for (let col = 0; col<columns; col++){
-            const currentCell = parsedAxiosData[row][col]
-            if( currentCell === '#'){
-                let coords;
+'use strict';
+
+const { getLongestRow } = require('./getLongestRow');
+
+exports.parseGrid = (parsedAxiosData) => {
+    const rows = parsedAxiosData.length;
+    const columns = getLongestRow(parsedAxiosData);
+    const aliveCells = [];
+    for (let row = 0; row < rows; row++) {
+        const columns = parsedAxiosData[row].length;
+        for (let col = 0; col < columns; col++) {
+            const currentCell = parsedAxiosData[row][col];
+            if ( currentCell === '#') {
                 // coords as they will be retrieved backwards
-                coords = [col, row];
-                aliveCells.push(coords)
+                const coords = [col, row];
+                aliveCells.push(coords);
             }
         }
     }
-    return {columns, rows, aliveCells}
-})
+
+    return { columns, rows, aliveCells };
+};
