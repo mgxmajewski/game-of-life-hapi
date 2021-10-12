6154cb26f63eaa307254544e5832121c2d3d5e0b
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-10-12 20:36:54 +0200

    Refactor parseGrid helper.

--- helpers/parseGrid.js
+++ helpers/parseGrid.js
@@ -2,12 +2,17 @@
 
 const { getLongestRow } = require('./getLongestRow');
 
+const numberOfRows = (parsedAxiosData) => parsedAxiosData.length;
+
 exports.parseGrid = (parsedAxiosData) => {
-    const rows = parsedAxiosData.length;
+
+    const rows = numberOfRows(parsedAxiosData);
     const columns = getLongestRow(parsedAxiosData);
     const aliveCells = [];
+    // eslint-disable-next-line @hapi/for-loop
     for (let row = 0; row < rows; row++) {
-        const columns = parsedAxiosData[row].length;
+        // const columns = parsedAxiosData[row].length;
+        // eslint-disable-next-line @hapi/for-loop
         for (let col = 0; col < columns; col++) {
             const currentCell = parsedAxiosData[row][col];
             if ( currentCell === '#') {
