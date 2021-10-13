eb31acc62c6f963f77d9f3a395cdc58c5b0afcab
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-10-13 21:55:06 +0200

    Adjust getLongestRow to linter.

--- helpers/getLongestRow.js
+++ helpers/getLongestRow.js
@@ -1,9 +1,14 @@
-exports.getLongestRow = grid => {
+'use strict';
+
+exports.getLongestRow = (grid) => {
+
     let max = 0;
-    for (let row =0; row < grid.length; row++){
-        if(max < grid[row].length){
-            max = grid[row].length
+    // eslint-disable-next-line @hapi/for-loop
+    for (let row = 0; row < grid.length; row++) {
+        if (max < grid[row].length) {
+            max = grid[row].length;
         }
     }
-    return max
-}
+
+    return max;
+};
