c9948b8142b4ef21b4baa28c2d01f0432aeeaa0f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-10-18 18:23:40 +0200

    Refactor getLongestRow helper function.

--- helpers/getLongestRow.js
+++ helpers/getLongestRow.js
@@ -2,13 +2,14 @@
 
 exports.getLongestRow = (grid) => {
 
-    let max = 0;
+    let maxRowLength = 0;
     // eslint-disable-next-line @hapi/for-loop
     for (let row = 0; row < grid.length; row++) {
-        if (max < grid[row].length) {
-            max = grid[row].length;
+        const currentRowLength = grid[row].length;
+        if (maxRowLength < currentRowLength) {
+            maxRowLength = currentRowLength;
         }
     }
 
-    return max;
+    return maxRowLength;
 };
