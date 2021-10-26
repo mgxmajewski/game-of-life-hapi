862dfa59250e27c4b2355b3daa330b427d0d9869
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-10-26 21:07:15 +0200

    Lint authPlugin.

--- auth/index.js
+++ auth/index.js
@@ -1,18 +1,20 @@
 'use strict';
+
 const Connection = require('../dbConfig');
 
 const authPlugin = {
     register: function (server, options) {
+
         module.exports.validate = async function (decoded, request, h) {
             // do your checks to see if the person is valid
             if (!await Connection.useModel.findByPk(decoded.id)) {
                 return { isValid: false };
             }
-            else {
-                return { isValid: true };
-            }
+
+            return { isValid: true };
+
         };
     },
     name: 'authPlugin'
-}
-module.exports = authPlugin
+};
+module.exports = authPlugin;
