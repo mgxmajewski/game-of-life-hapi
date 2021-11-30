cd860834a7219c2791e4bd6ac064382483fdc4d8
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-11-30 11:30:53 +0100

    Create object to store separate interval for each user.

--- grid_utils/intervalSetter.js
+++ grid_utils/intervalSetter.js
@@ -1,15 +1,20 @@
 'use strict';
 
-let interval;
+const { getTokenId } = require('../utils/getTokenId');
+
+const intervals = {};
+
 /**
  * @param {function(*=): Promise<void>} cb - function which will be invoked in interval.
  * @param {number} timeOut - length of time interval given in milliseconds.
  * @param token
  */
+
 exports.updateInterval = (cb, timeOut, token) => {
 
-    clearInterval(interval);
+    const interval = getTokenId(token);
+    clearInterval(intervals[interval]);
     if (timeOut.toString() !== '101') {
-        interval = setInterval(() => cb(token), timeOut);
+        intervals[interval] = setInterval(() => cb(token), timeOut);
     }
 };
